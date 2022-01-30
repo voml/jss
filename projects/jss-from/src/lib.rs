fac8cd035303f667c4bb469aba84ad3891783533
Aster <galaster@foxmail.com>
2022-01-31 07:45:27 +0800

    🔮 Add pest parser

--- projects/jss-from/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![doc = include_str!("../Readme.md")]
-
-pub(crate) mod json_schema;
-pub(crate) mod validation;
-
-pub use json_schema::*;
---
+++ projects/jss-pest/src/lib.rs
@@ -0,0 +1,10 @@
+mod arc;
+
+pub use arc::{ArcParser, Rule};
+pub use pest::{
+    self,
+    error::Error,
+    iterators::{Pair, Pairs},
+    prec_climber::{Assoc, Operator, PrecClimber},
+    Parser, Span,
+};
