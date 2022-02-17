5f1257b99c64821cc5c082becbc35b02e3d2fb85
Aster <galaster@foxmail.com>
2022-02-17 09:25:33 +0800

    🔮 Add js support

--- projects/jss-core/src/from/from_json/mod.rs
+++ projects/jss-core/src/from/from_json/mod.rs
@@ -1,5 +1,14 @@
 mod parse_main;
 mod parse_type;
 
-use crate::{Errors, JssError, Result};
+use crate::{Errors, JssError, JssSchema, Result};
 use json_value::JsonValue;
+
+impl TryFrom<JsonValue> for JssSchema {
+    type Error = JssError;
+
+    fn try_from(value: JsonValue) -> Result<Self> {
+        let mut errors = vec![];
+        Self::try_parse_json_schema(value, &mut errors)
+    }
+}
