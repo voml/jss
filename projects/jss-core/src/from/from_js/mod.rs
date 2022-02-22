f3d3c55256738f785c25f22e84bfba7c0f29d7b3
Aster <galaster@foxmail.com>
2022-02-22 20:50:12 +0800

    🔧 Fix js error panic

--- projects/jss-core/src/from/from_js/mod.rs
+++ projects/jss-core/src/from/from_js/mod.rs
@@ -1,23 +1,29 @@
-use crate::{JssError, JssSchema};
+use crate::JssSchema;
 use json_value::JsonValue;
 use serde_wasm_bindgen::from_value;
 use wasm_bindgen::{JsError, JsValue};
 
 impl TryFrom<JsValue> for JssSchema {
-    type Error = JssError;
+    type Error = JsError;
 
     fn try_from(js: JsValue) -> Result<Self, Self::Error> {
-        let value: JsonValue = from_value(js)?;
-        return JssSchema::try_from(value);
+        let value = match from_value::<JsonValue>(js) {
+            Ok(o) => o,
+            Err(e) => return Err(JsError::from(e)),
+        };
+        match JssSchema::try_from(value) {
+            Ok(o) => Ok(o),
+            Err(e) => Err(JsError::from(e)),
+        }
     }
 }
 
 impl JssSchema {
-    pub fn validate_js(&self, object: JsValue) -> Vec<JsError> {
+    pub fn validate_js(&self, object: JsValue) -> Vec<JsValue> {
         let value = match from_value::<JsonValue>(object) {
             Ok(o) => o,
-            Err(e) => return vec![JsError::from(e)],
+            Err(e) => return vec![JsValue::from(JsError::from(e))],
         };
-        return self.validate(&value).into_iter().map(JsError::from).collect();
+        return self.validate(&value).into_iter().map(|e| JsValue::from(JsError::from(e))).collect();
     }
 }
--- projects/jss-wasm/src/lib.rs
+++ projects/jss-wasm/src/lib.rs
@@ -1,11 +1,9 @@
 #![doc = include_str!("../Readme.md")]
 
-use serde_json::Value;
-use serde_wasm_bindgen::from_value;
 use std::str::FromStr;
-use wasm_bindgen::{prelude::*, JsObject};
+use wasm_bindgen::prelude::*;
 
-use jss_core::{JssError, JssSchema};
+use jss_core::JssSchema;
 
 #[wasm_bindgen]
 pub struct Jss {
@@ -16,31 +14,32 @@ pub struct Jss {
 impl Jss {
     /// Create a new JSS instance.
     #[wasm_bindgen(constructor)]
-    pub fn from_string(jss: &str) -> Result<Self, JsError> {
-        let jss = JssSchema::from_str(jss)?;
-        Ok(Self { internal: jss })
+    pub fn from_string(jss: &str) -> Result<Jss, JsError> {
+        Ok(Self { internal: JssSchema::from_str(jss)? })
     }
 
     /// Get the JSS instance.
-    pub fn from_json_schema(object: JsValue) -> Result<Self, JsError> {
-        let jss = JssSchema::try_from(object)?;
-        Ok(Self { internal: jss })
+    pub fn from_json_schema(object: JsValue) -> Result<Jss, JsError> {
+        Ok(Self { internal: JssSchema::try_from(object)? })
     }
 
     /// Get the JSS instance.
-    pub fn validate(&self, object: JsValue) -> Result<Vec<JsError>, JsError> {
-        let value: Value = from_value(object)?;
-        Ok(self.internal.validate(&value))
+    pub fn validate(&self, object: JsValue) -> Vec<JsValue> {
+        self.internal.validate_js(object)
     }
 
     /// Get the JSS instance.
-    pub fn is_valid(&self, object: JsValue) -> Result<bool, JsError> {
-        let value: Value = from_value(object)?;
-        Ok(self.internal.is_valid(&value))
+    pub fn is_valid(&self, object: JsValue) -> bool {
+        self.internal.validate_js(object).is_empty()
     }
 
     /// Get random schema
     pub fn mock(&self) -> JsValue {
         unimplemented!("random")
     }
+}
+
+#[test]
+fn ready() {
+    println!("it works!")
 }
