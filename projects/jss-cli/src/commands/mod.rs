35460e97d2c93cb47dd307b777994c2c3dd73ae5
Aster <galaster@foxmail.com>
2022-03-08 18:33:00 +0800

    🔮 Use cli scaffolding

---
+++ projects/jss-cli/src/commands/mod.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+
+use clap::Subcommand;
+
+use tailwind_rs::{GlobalConfig, Result};
+
+#[derive(Subcommand)]
+pub enum TailwindCommands {
+    Init {
+        #[clap(parse(from_os_str), value_name = "DIR")]
+        workspace: Option<PathBuf>,
+    },
+}
+
+impl TailwindCommands {
+    pub fn run(&self, config: &GlobalConfig) -> Result<()> {
+        let _ = config;
+        println!("?");
+        match self {
+            Self::Init { workspace } => {
+                println!("'myapp add' was used, name is: {:?}", workspace)
+            }
+        }
+        Ok(())
+    }
+}
